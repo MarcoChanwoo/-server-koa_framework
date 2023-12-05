011486923fc47f3f64dee341392f67db258e7dc5
Marco <cwhong8087@gmail.com>
2023-12-05 23:54:30 +0900

    “updated”

--- src/models/post.js
+++ src/models/post.js
@@ -5,5 +5,12 @@ const { Schema } = mongoose;
 const PostSchema = new Schema({
   title: String,
   body: String,
-  tags: [String],
+  tags: [String], // 문자열로 이뤄진 배열
+  publishedDate: {
+    type: Date,
+    default: Date.now, // 현 날짜를 기본값으로 지정
+  },
 });
+
+const Post = mongoose.model('Post', PostSchema);
+export default Post;
