1819fdd1bf147b75ec59da02543454789a8cd327
Marco <cwhong8087@gmail.com>
2023-12-07 16:05:13 +0900

    “updated”

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -1,5 +1,25 @@
+import Joi from 'joi';
+
+/*
+  POST /api/auth/register
+  {
+    username: 'marco',
+    password: 'marco12'
+  }
+*/
 export const register = async (ctx) => {
   // 회원가입
+  // Request Body 검증하기
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
 };
 
 export const login = async (ctx) => {
